5ab668e697ad98150074af0f414065ba0fae623a
mikeunge <mikeunge@protonmail.com>
2022-01-09 19:41:08 +0100

    add: uploading csv's and detecting changes works but at the same time not really, way to in-efficient and we lose the prev. data

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -66,6 +66,7 @@ class GamesController extends AbstractController
 									continue;
 								}
 							}
+							
 							// catch errors
 							if ($error) {
 								break;
@@ -73,40 +74,75 @@ class GamesController extends AbstractController
 								continue;
 							}
 
-							// check if game and needed entities exist
 							$games = $gamesRepository->findByTitle($data[0]);
-							$developer = $developersRepository->findByName($data[2]);
-							$genre = $genresRepository->findByName($data[3]);
-							$plattform = $plattformsRepository->findByName($data[4]);
-
-							// create new entity or use first element from returned entity array
-							if (count($developer) == 0) {
-								$developer = new Developers();
-								$developer->setName($data[2]);
-								$entityManager->persist($developer);
-							} else {
-								$developer = $developer[0];
-							}
-							if (count($genre) == 0) {
-								$genre = new Genres();
-								$genre->setName($data[3]);
-								$entityManager->persist($genre);
-							} else {
-								$genre = $genre[0];
-							}
-							if (count($plattform) == 0) {
-								$plattform = new Plattforms();
-								$plattform->setName($data[3]);
-								$entityManager->persist($plattform);
-							} else {
-								$plattform = $plattform[0];
-							}
-
-							if (count($games) > 0) {
-								// need to do some updating
-								continue;
-							} else {
-								// create a new game
+							if (count($games) > 0) {	// need to do some updating
+								$games = $games[0];
+								if ($games->getDescription() != $data[1]){
+									$games->setDescription($data[1]);
+								}
+								if (!in_array($data[2], array($games->getDeveloper()))) {
+									$developer = $developersRepository->findByName($data[2]);
+									if (count($developer) == 0) {
+										$developer = new Developer();
+										$developer->setName($data[2]);
+										$entityManager->persist($developer);
+									}
+									$games->setDeveloper($developer);
+								}
+								if (!in_array($data[3], array($games->getGenre()))) {
+									$genres = $genresRepository->findByName($data[3]);
+									if (count($genres) == 0) {
+										$genres = new Genre();
+										$genres->setName($data[3]);
+										$entityManager->persist($genres);
+									}
+									foreach ($genres as $genre) {
+										$games->addGenre($genre);
+									}
+								}
+								if (!in_array($data[4], array($games->getPlattform()))) {
+									$plattforms = $plattformsRepository->findByName($data[4]);
+									if (count($plattforms) == 0) {
+										$plattforms = new Plattforms();
+										$plattforms->setName($data[4]);
+										$entityManager->persist($plattforms);
+									}
+									foreach ($plattforms as $plattform) {
+										$games->addPlattform($plattform);
+									}
+								}
+								if ($games->getImage() != $data[5]) {
+									$games->setImage($data[5]);
+								}
+								$entityManager->persist($games);
+								$entityManager->flush();
+							} else {	// create a new game
+								// check if needed entities exist
+								$developer = $developersRepository->findByName($data[2]);
+								$genre = $genresRepository->findByName($data[3]);
+								$plattform = $plattformsRepository->findByName($data[4]);
+								// create new entity or use existing
+								if (count($developer) == 0) {
+									$developer = new Developers();
+									$developer->setName($data[2]);
+									$entityManager->persist($developer);
+								} else {
+									$developer = $developer[0];
+								}
+								if (count($genre) == 0) {
+									$genre = new Genres();
+									$genre->setName($data[3]);
+									$entityManager->persist($genre);
+								} else {
+									$genre = $genre[0];
+								}
+								if (count($plattform) == 0) {
+									$plattform = new Plattforms();
+									$plattform->setName($data[3]);
+									$entityManager->persist($plattform);
+								} else {
+									$plattform = $plattform[0];
+								}
 								$game = new Games();
 								$game->setTitle($data[0]);
 								$game->setDescription($data[1]);
